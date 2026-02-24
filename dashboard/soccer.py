a2e4598ef8a5d1ed5830532755d94e3de4d996b8
ankitgoyala07 <ankitgoyalao7@gmail.com>
2026-02-24 11:51:30 +0545

    background image added

--- dashboard/soccer.py
+++ dashboard/soccer.py
@@ -1,26 +1,33 @@
-
 import tkinter as tk
+from PIL import Image, ImageTk
 
 # Create main window
 root = tk.Tk()
-root.title("Soccer Management System")
+root.title("Manager Dashboard")
 root.geometry("500x450")
 root.config(bg="#f0f0f0")
 
-# Title Label
-title = tk.Label(root, text="Wellcome",font=("Arial", 22, "bold"),bg="#f0f0f0")
+image = Image.open("image.png")
+image = image.resize((500, 450))
+bg_image = ImageTk.PhotoImage(image)
+
+# Create a label for the background
+bg_label = tk.Label(root, image=bg_image)
+bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+title = tk.Label(root, text="Dashboard",font=("Arial", 22, "bold"),bg="#f0f0f0")
 title.pack(pady=30)
 
-# Buttons (No Command Added)
-btn_player = tk.Button(root, text="Player Management",width=25, height=2,font=("Arial", 12))
+btn_player = tk.Button(root, text="Player Management",width=25, height=2,font=("Arial", 12), bg="#39268B", fg="white")
 btn_player.pack(pady=10)
-btn_team = tk.Button(root, text="Team Management",width=25, height=2,font=("Arial", 12))
+
+btn_team = tk.Button(root, text="Team Management",width=25, height=2,font=("Arial", 12), bg="#39268B", fg="white")
 btn_team.pack(pady=10)
 
-btn_match = tk.Button(root, text="Match Management",width=25, height=2,font=("Arial", 12))
+btn_match = tk.Button(root, text="Match Management",width=25, height=2,font=("Arial", 12), bg="#39268B", fg="white")
 btn_match.pack(pady=10)
 
-btn_tournament = tk.Button(root, text="Tournament Management",width=25, height=2,font=("Arial", 12))
+btn_tournament = tk.Button(root, text="Tournament Management",width=25, height=2,font=("Arial", 12), bg="#39268B", fg="white")
 btn_tournament.pack(pady=10)
 
 root.mainloop()
